8ac1fd278b442a62eb5454288866dc73b1ad1d1e
satyamyendhe <satyamyendhe7@gmail.com>
2023-03-23 21:00:09 +0530

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+dataBase  = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'root'
+
+)
+
+# cursor Object
+cursorObject = dataBase.cursor()
+
+# create DB
+cursorObject.execute("CREATE DATABASE CRM;")
+
+print("Database Created")

ec3d870c9703918ec90da4e3ba57192b9e41d0e5
satyamyendhe <satyamyendhe7@gmail.com>
2023-03-24 00:30:08 +0530

    Done Login function

--- website/views.py
+++ website/views.py
@@ -1,4 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
 def home(request) :
-    return render(request, 'home.html', {})
+    # check to see if logging in
+    if request.method == 'POST':
+        username = request.POST['username'] 
+        password = request.POST['password']
+        # authenticate
+        user = authenticate(request, username=username,  password=password)
+        if user is not None :
+            login(request , user)
+            messages.success(request,"You have been Logged In!")
+            return redirect('home')
+        else:
+            messages.success(request , "There was an error Logging In , Please try again....")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+# def login_user(request):
+#     pass
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,"You have been logged out....")
+    return redirect('home')

3499ed8e60b114e8815d5794607b4c84aea2db38
satyamyendhe <satyamyendhe7@gmail.com>
2023-03-25 16:51:35 +0530

    Final Project

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm ,AddRecordForm
 from .models import Record
 
 def home(request) :
@@ -74,4 +74,30 @@ def delete_record(request , pk):
 
 
 def add_record(request):
-    return render(request, 'add_record.html', {})
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Record Added...")
+                return redirect('home')
+
+        return render(request, 'add_record.html', {'form':form})
+    else : 
+        messages.success(request, "You must be Logged In...")
+        return redirect('home')
+    
+def update_record(request,pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None , instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Record has been Updated!")
+            customer_record = Record.objects.get(id=pk)
+            return render(request, 'record.html', {'customer_record':customer_record})
+            # return redirect('home') 
+        return render(request, 'update_record.html',{'form':form })
+    else :
+        messages.success(request, "You must be Logged In...")
+        return redirect('home')
